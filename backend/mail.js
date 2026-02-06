23e7b242499b73bc0cf0487d65e1ba84b00a55ad
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2026-02-07 03:20:23 +0400

    fix

--- backend/mail.js
+++ backend/mail.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: 587,
   secure: "false",
   auth: {
     user: process.env.SMTP_USER,
