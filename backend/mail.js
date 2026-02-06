1a4be1ac3555c54ddc44e7831750ffae38ff64ea
Huseyn Tapdigli <74926067+huseynt@users.noreply.github.com>
2026-02-07 03:32:00 +0400

    fix

--- backend/mail.js
+++ backend/mail.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-import dns from "dns";
+const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
